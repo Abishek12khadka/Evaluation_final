d62eda07ae15c02b73ea0d82fe755052f83c7499
Jemyjogi <jemyleah1303@gmail.com>
2024-01-25 14:43:30 +0530

    new pgm

--- add.c
+++ add.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
-    int num1, num2, num3, num4 add;
-=======
     int num1, num2, num3, num4, add;
->>>>>>> acaf3f429cdf2fbbc5e57ae07994bf3f3f18e8cc
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -16,13 +12,12 @@ int main() {
     printf("Enter the third number: ");
     scanf("%d", &num3);
 
-<<<<<<< HEAD
      printf("Enter the fourth number: ");
     scanf("%d", &num4);
-=======
+
     printf("Enter the fourth number: ");
     scanf("%d", &num3);
->>>>>>> acaf3f429cdf2fbbc5e57ae07994bf3f3f18e8cc
+
     
     add = num1 + num2 + num3 + num4 + num5;
     
