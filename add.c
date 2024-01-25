edc94c44386dc0992a77f7a008bc4f3c47aaccb1
Abishek khadka <155621375+Abishek12khadka@users.noreply.github.com>
2024-01-25 14:14:40 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sub;
+    int num1, num2, num3, sub;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
     scanf("%d", &num2);
+
+    printf("Enter the third number: ");
+    scanf("%d", &num3);
     
-    sub = num1 - num2;
+    sub = num1 + num2 + num3;
     
-    printf("The sub of %d and %d is %d.", num1, num2, sum);
+    printf("The sub of %d and %d is %d.", num1, num2, num3, sum);
     
     return 0;
 }
